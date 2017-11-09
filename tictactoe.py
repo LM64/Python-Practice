fe74bb1f55b2b448bb57616df3c4d4d17c65e0bc
Logan Macias (Class of '20) <loganmacias@527-imac23.admin.sfhs.com>
2017-11-09 15:53:24 -0800

    First commmit

---
+++ tictactoe.py
@@ -0,0 +1,21 @@
+#Game Board
+matrix = [["O","X","O"],["X","O","X"],["O","X","O"]]
+#for x in matrix:
+    #for i in x:
+    #    print i
+    #print
+x = 0
+
+while x < len(matrix):
+    #print(matrix[x])
+    i = 0
+
+    while i < len(matrix):
+        print matrix[x][i]
+        i += 1
+    x += 1
+    print
+
+
+#lst1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "nintendo64"]
+#print lst1[]
